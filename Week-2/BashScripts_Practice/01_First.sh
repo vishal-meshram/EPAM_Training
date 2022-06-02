47b970beb4791edde24e386bbd480ce481b7da45
Vishal Meshram <vishal.meshram@viit.ac.in>
2022-06-02 14:45:38 +0530

    Create 01_First.sh

---
+++ Week-2/BashScripts_Practice/01_First.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This is Hello World script.
+#This script will print message on screen 
+#Note: First change the perssion of script by using command: chmod u+x 01_First.sh
+
+echo "Hello Bash"
+
+str1="Hello"
+echo "str1 bash"
+
+str1="Hello"
+str2="Bash"
+echo "$str1 $str2"
+
+
+
+#Author: Vishal
